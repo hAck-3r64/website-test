c684a66bd90098baa10be03476357398b48257ff
hAck_3r64 <83057120+hAck-3r64@users.noreply.github.com>
2023-05-08 12:48:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,32 +10,3 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
-
-const track = document.getElementById("image-track");
-
-window.onmousedown = e => {
-  track.dataset.mouseDownAt = e.clientX;
-}
-
-window.onmouseup = () => {
-  track.dataset.mouseDownAt = "0";
-  track.dataset.prevPercentage = track.dataset.percentage;
-}
-
-window.onmousemove = e => {
-  if(track.dataset.mouseDownAt === "0") return;
-  
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-  
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-  
-  Math.min(nextPercentage, 0);
-  Math.max(nextPercentage, -100);
-  
-  track.dataset.percentage = nextPercentage;
-  
-  track.style.transform = `translate(${percentage}%, -50%)`;
-}
